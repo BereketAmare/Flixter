e512029638e0522b196082ec9e142bcfe82ee5b0
Bereket Nigussie <48007960+BereketAmare@users.noreply.github.com>
2022-03-10 00:22:51 -0500

    Added flicter Grid View Feature

---
+++ flicter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flicter
+//
+//  Created by Bereket Nigussie on 3/9/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
